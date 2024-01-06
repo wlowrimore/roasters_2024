a2eaad4fe8bc744234e0ad7a325558a10af41a37
William Lowrimore <wlowrimore@gmail.com>
2024-01-05 22:17:10 -0600

    most styling finished working on nav

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,8 +4,8 @@ import SmilingCoffee from '/public/images/test-images/smiling_lady_drinking_coff
 const HeroSection = () => {
   return (
     <div className='flex flex-col md:flex-row md:mx-4 lg:mb-1'>
-      <div className='w-full md:h-72 lg:h-[22rem] pt-14 pb-4 flex px-4 lg:px-12 flex-col justify-center'>
-        <h1 className='uppercase tracking-wide font-bold text-neutral-600 text-4xl lg:text-5xl leading-8 mb-2 '>International Coffee Roasters</h1>
+      <div className='w-full md:h-72 lg:h-[22rem] pt-14 xl:pt-0 pb-4 flex px-4 lg:px-12 flex-col justify-center'>
+        <h1 className='uppercase tracking-wide font-bold text-neutral-600 text-4xl lg:text-4xl leading-8 mb-2 lg:mb-0 '>International Coffee Roasters</h1>
         <h2 className='tracking-wide font-semibold text-neutral-600 text-xl lg:text-2xl leading-6 mb-2'>Fair Trade. Indigenous Cacao. Right Price.</h2>
         <p className='text-sm lg:text-lg tracking-wide lg:tracking-normal lg:leading-6'>Don&apos;t pay high prices for low quality java.  We eliminate the middle-man so we can bring to you, from the farm, freshly harvested coffee, custom roasted to your specifications.</p>
       </div>
@@ -18,7 +18,7 @@ const HeroSection = () => {
           height={1000}
           className='md:object-cover md:h-72 lg:h-[22rem] lg:pr-12'
         />
-        <div className='absolute z-10 top-6 md:top-14 left-4 lg:left-8 flex flex-col text-white/60'>
+        <div className='absolute z-10 top-6 md:top-14 lg:top-[4.2rem] left-4 lg:left-8 flex flex-col text-white/60'>
           <h3 className='text-lg lg:text-2xl font-bold'>100&#37; Organic</h3>
           <ul className='list-disc ml-5 lg:text-lg'>
             <li>Robusta</li>
--- src/app/components/MobileMenu.jsx
+++ src/app/components/MobileMenu.jsx
@@ -15,31 +15,48 @@ const MobileMenu = () => {
 
   return (
     <main>
-      <button
-        onClick={handleClick}
-        className='w-28 bg-yellow-700 py-1 px-3 rounded-br-lg shadow shadow-yellow-900 text-[#EFCA99] text-lg absolute z-10 top-0 left-0 uppercase'
-      >
-        Menu
-      </button>
-      {isOpen ? (
-        <div className='w-full h-[32rem] absolute top-0 z-20 bg-gradient-to-b from-yellow-900 to-yellow-800 shadow-xl shadow-yellow-900'>
-          <button onClick={handleClick} className='bg-yellow-700 text-[#efca99] text-lg text-start py-1 px-7 uppercase rounded-br-lg'>Close</button>
-          <div className='text-[#efca99] px-5 space-y-2 my-4'>
-            <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Home</p></Link>
-            <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Our Story</p></Link>
-            <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>About Our Cacao</p></Link>
-            <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Roasting Options</p></Link>
-            <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Price Guide</p></Link>
-            <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Fun Facts</p></Link>
-            <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>FAQ</p></Link>
-            <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Contact</p></Link>
-            <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Privacy Policy</p></Link>
-            <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Terms & Conditions</p></Link>
+      <section
+        className='lg:hidden'>
+        <button
+          onClick={handleClick}
+          className='w-28 bg-yellow-700 py-1 px-3 rounded-br-lg shadow shadow-yellow-900 text-[#EFCA99] text-lg absolute z-10 top-0 left-0 uppercase'
+        >
+          Menu
+        </button>
+        {isOpen ? (
+          <div className='w-full h-[32rem] absolute top-0 z-20 bg-gradient-to-b from-yellow-900 to-yellow-800 shadow-xl shadow-yellow-900'>
+            <button onClick={handleClick} className='bg-yellow-700 text-[#efca99] text-lg text-start py-1 px-7 uppercase rounded-br-lg'>Close</button>
+            <div className='text-[#efca99] px-5 space-y-2 my-4'>
+              <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Home</p></Link>
+              <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Our Story</p></Link>
+              <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>About Our Cacao</p></Link>
+              <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Roasting Options</p></Link>
+              <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Price Guide</p></Link>
+              <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Fun Facts</p></Link>
+              <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>FAQ</p></Link>
+              <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Contact</p></Link>
+              <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Privacy Policy</p></Link>
+              <Link href='#'><p className='active:bg-yellow-900/90 w-[13rem] rounded-r-lg rounded-l-md px-2 py-1'>Terms & Conditions</p></Link>
+            </div>
           </div>
-        </div>
-      ) : (
-        null
-      )}
+        ) : (
+          null
+        )}
+      </section>
+
+      {/* large screens navigation */}
+      <section>
+        <nav className='hidden lg:block fixed top-1 lg:left-0 xl:left-24'>
+          <ul className='flex items-center w-full h-[2rem] mx-12 uppercase'>
+            <li><Link href='#' className='py-2 px-4'>ICR</Link></li>
+            <li><Link href='#' className='py-2 px-4'>Pricing</Link></li>
+            <li><Link href='#' className='py-2 px-4'>Process</Link></li>
+            <li><Link href='#' className='py-2 px-4'>Our Story</Link></li>
+            <li><Link href='#' className='py-2 px-4'>Contact</Link></li>
+          </ul>
+        </nav>
+      </section>
+
     </main>
   )
 }
